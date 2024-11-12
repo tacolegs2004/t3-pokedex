a2e220b477414f4fb1cd040c02ffc12fcbb68128
taco boi <tylerchall16@gmail.com>
2024-11-12 16:13:36 -0600

    did a lot of stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -14,7 +15,16 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body className="bg-gradient-to-r from-[#b71e1e] to-[#fdff7c] text-white">
+        <header className="-mb-8 mt-4 flex items-center justify-center">
+          <Link href="/" className="text-2xl font-bold text-black">
+            T3 Pokedex
+          </Link>
+        </header>
+        <main className="flex min-h-screen flex-col items-center justify-center">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
---
+++ src/app/pokemon/[pokemonId]/page.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { PokemonClient } from "pokenode-ts";
+
+const pokemonClient = new PokemonClient();
+
+async function PokemonPage({ params }: { params: { pokemonId: string } }) {
+  const pokemon = await pokemonClient.getPokemonById(Number(params.pokemonId));
+
+  if (!pokemon) {
+    return <div>Pokemon not found.</div>;
+  }
+
+  return (
+    <div className="container flex w-[80%] flex-col items-center justify-center gap-4 p-4">
+      <div className="flex flex-col items-center justify-center rounded-lg bg-white p-6 text-black shadow-lg">
+        <h1 className="mb-4 text-center text-3xl font-bold capitalize">
+          {pokemon.name}
+        </h1>
+        <Image
+          src={pokemon.sprites.front_default ?? "/placeholder.png"}
+          alt={`${pokemon.name}'s sprite`}
+          width={120}
+          height={120}
+        />
+        <div className="mt-4 text-center">
+          <p className="text-lg">
+            <strong>Type:</strong>{" "}
+            {pokemon.types.map((type) => type.type.name).join(", ")}
+          </p>
+          <p className="text-lg">
+            <strong>Height:</strong> {pokemon.height / 10} m
+          </p>
+          <p className="text-lg">
+            <strong>Weight:</strong> {pokemon.weight / 10} kg
+          </p>
+          <p className="text-lg">
+            <strong>Abilities:</strong>{" "}
+            {pokemon.abilities
+              .map((ability) => ability.ability.name)
+              .join(", ")}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PokemonPage;
